975526b693927e23ca457ddf9d02b7e336e0969d
Joe Taylor <joe@textninja.net>
2022-07-16 10:02:05 -0700

    use full screen, circles not characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,74 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { conway } from './conway';
+import { CELL_SIZE } from './constants';
+import './App.scss';
+
+function getWindowDimensions() {
+  const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
+
+  return { windowWidth, windowHeight };
+}
 
 function App() {
+  const [{ windowWidth, windowHeight }, setWindowDimensions] = useState(getWindowDimensions())
+  
+  const [cols, setCols] = useState(windowWidth/CELL_SIZE|0);
+  const [rows, setRows] = useState(windowHeight/CELL_SIZE|0);
   const zeroOrOne = () => Number(Math.random() > 0.5);
-  const [grid, setGrid] = useState(
-    Array(20).fill(0).map(row => (
-      Array(20).fill(0).map(zeroOrOne)
+  const randgrid = (r, c) => (
+    Array(rows).fill(0).map(() => (
+      Array(cols).fill(0).map(zeroOrOne)
     ))
   );
+  const [grid, setGrid] = useState(randgrid(rows, cols));
+
+  useEffect(() => {
+    const resize = () => {
+      setCols(window.innerWidth/CELL_SIZE|0);
+      setRows(window.innerHeight/CELL_SIZE|0);
+    };
+
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  useEffect(() => {
+    setGrid(randgrid(rows, cols));
+  }, [rows, cols])
 
   useEffect(() => {
     const i = setInterval(
       () => setGrid(g => conway(g)),
-      200
+      150
     );
 
     return () => clearInterval(i);
   }, []);
 
   return (
-    grid.map(row => <Row data={row}/>)
+    grid.map((row, rowNum) => (
+      <Row key={rowNum}>
+        {row.map((cell, cellNum) => <Cell size={CELL_SIZE} state={cell}/>)}
+      </Row>
+    ))
   );
 }
 
-function Row({ data }) {
+function Row({ children }) {
   return (
-    <pre>{data.map(c => c ? '@' : String.fromCharCode(160)).join(' ')}</pre>
+    <div>{ children }</div>
   );
+}
+
+function Cell({ size, state }) {
+  return (
+    <div style={{
+      width: `${size}px`,
+      height: `${size}px`,
+      backgroundColor: state ? "silver" : "white",
+      borderRadius: "20px"
+    }}></div>
+  )
 }
 
 export default App;
